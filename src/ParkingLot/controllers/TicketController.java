228fc269204948f2837d48c254c9d054b3725c05
nikhildommaraju <nikhildommaraju145@gmail.com>
2026-06-30 08:04:01 +0530

    ParkingLot:TicketController

---
+++ src/ParkingLot/controllers/TicketController.java
@@ -0,0 +1,28 @@
+package ParkingLot.controllers;
+
+import ParkingLot.dtos.IssueTicketRequest;
+import ParkingLot.dtos.IssueTicketResponse;
+import ParkingLot.dtos.ResponseStatus;
+import ParkingLot.models.Ticket;
+import ParkingLot.services.TicketService;
+
+public class TicketController {
+    private TicketService ticketService;
+
+    public TicketController(TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+
+    public IssueTicketResponse issueTicket(IssueTicketRequest request){
+        IssueTicketResponse response = new  IssueTicketResponse();
+        try{
+            Ticket ticket = ticketService.issueTicket();
+            response.setTicketId(ticket.getId());
+            response.setResponseStatus(ResponseStatus.SUCCESS);
+        }catch(Exception ex){
+            response.setResponseStatus(ResponseStatus.FAILURE);
+            response.setFailureMessage(ex.getMessage());
+        }
+        return response;
+    }
+}
---
+++ src/ParkingLot/dtos/IssueTicketRequest.java
@@ -0,0 +1,37 @@
+package ParkingLot.dtos;
+
+public class IssueTicketRequest {
+    private String licensePlateNumber;
+    private String ownerName;
+    private int gateId;
+
+    public IssueTicketRequest(String licensePlateNumber, String ownerName, int gateId) {
+        this.licensePlateNumber = licensePlateNumber;
+        this.ownerName = ownerName;
+        this.gateId = gateId;
+    }
+
+    public String getLicensePlateNumber() {
+        return licensePlateNumber;
+    }
+
+    public void setLicensePlateNumber(String licensePlateNumber) {
+        this.licensePlateNumber = licensePlateNumber;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public int getGateId() {
+        return gateId;
+    }
+
+    public void setGateId(int gateId) {
+        this.gateId = gateId;
+    }
+}
